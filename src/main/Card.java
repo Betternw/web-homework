e4ef164f6a6f7696c046e382b6962e32396eb3f6
Betternw <lhz0627@stu.xjtu.edu.cn>
2020-03-13 20:59:55 +0800

    敏捷web第三周作业

---
+++ src/main/Card.java
@@ -0,0 +1,42 @@
+package main;
+
+/**
+ * 扑克牌类，有两个属性，点数num和花色type
+ */
+public class Card {
+    private Integer num;
+    private Character type;
+
+    public Card() {
+        super();
+    }
+
+    public Card(Integer num, Character type) {
+        this.num = num;
+        this.type = type;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Character getType() {
+        return type;
+    }
+
+    public void setType(Character type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "num=" + num +
+                ", type=" + type +
+                '}';
+    }
+}
---
+++ src/test/FirstTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import junit.framework.Assert;
+import main.TexasHoldem;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstTest {
+    @Test
+    public void testJudgeMethod() {
+        List<String> white = new ArrayList<>();
+        List<String> black = new ArrayList<>();
+        white.add("2H");
+        white.add("3D");
+        white.add("5S");
+        white.add("9C");
+        white.add("KD");
+        black.add("2D");
+        black.add("3H");
+        black.add("5C");
+        black.add("9S");
+        black.add("KH");
+        System.out.println(TexasHoldem.judgeCards(white,black));
+        Assert.assertEquals("Tie", TexasHoldem.judgeCards(white, black));
+    }
+}
